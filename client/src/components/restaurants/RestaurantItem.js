3aa73273eddb5d96c9aadcd57a7a7813e56786e0
Jessica <jessica.ho.jh@gmail.com>
2019-11-01 16:27:10 -0700

    create RestaurantItem component

---
+++ client/src/components/restaurants/RestaurantItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const RestaurantItem = ({ restaurant }) => {
+
+  return (
+    <div>
+        Item
+    </div>
+  );
+};
+
+
+export default connect(
+  null
+)(RestaurantItem);
