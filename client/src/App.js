fab94b7a44372e4c2fedf62fdd20f89f258a1550
Jessica <jessica.ho.jh@gmail.com>
2019-11-01 14:12:46 -0700

    set up redux store

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,27 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-
-      <Fragment>
-      <Navbar/>
-        <div className="container">
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/about' component={About}/>
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+        <Navbar/>
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/about' component={About}/>
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
