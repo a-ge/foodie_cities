073a5b4ab891deedb6befab453a12f1757780477
Ashley <amgesmundo@gmail.com>
2019-11-03 17:15:11 -0800

    Fixed seed to create objects in restaurants_data.json and subsequently had to fix passing the objects to restaurants file.

--- client/src/components/restaurants/Restaurants.js
+++ client/src/components/restaurants/Restaurants.js
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import RestaurantItem from './RestaurantItem';
+import restaurants_data from '../../restaurants_data.json';
 
-const Restaurants = ({ restaurants }) => {
-  useEffect(() => {
-  }, []);
-
+const Restaurants = ({ city }) => {
+  const city_restaurants = restaurants_data[city]
 
   return (
     <div>
-      {
-        restaurants.map(restaurant => <RestaurantItem restaurant={restaurant.name} key={restaurant.id} />)
-      }
+    item
+
     </div>
   );
 };
 
 export default Restaurants
+// <ul>
+//   {
+//     city_restaurants.map(restaurant =>
+//       <RestaurantItem restaurant={restaurant.name} key={restaurant.id} />)
+//   }
+//   </ul>
