4b202c692f6428409259e95a5a9250bf13b9e082
Jessica <jessica.ho.jh@gmail.com>
2019-11-06 13:52:17 -0800

    createstate for heart and toggleHeart function. also fixed CORS issue.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+var cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
