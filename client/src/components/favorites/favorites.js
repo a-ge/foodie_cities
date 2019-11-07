0e4c958b4e670cf618de08c90e0668b90981a3ab
Ashley <amgesmundo@gmail.com>
2019-11-06 17:26:18 -0800

    created fetchFave function.

--- client/src/components/favorites/favorites.js
+++ client/src/components/favorites/favorites.js
@@ -1,35 +1,42 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { getFavRestaurants } from '../../actions/restaurantActions';
 import axios from 'axios';
-
 
 const FavRestaurants = () => {
 
-    const [favorites, setFavorites] = useState([]);
-    const [checkHeart, setCheckHeart] = useState(false);
+  const [favorites, setFavorites] = useState([]);
+  const [checkHeart, setCheckHeart] = useState(false);
 
   const toggleHeart = () => {
-    axios.post('http://localhost:5000/favorites/add', 
-      {restaurant_id: 1, user_id: 2, status: "went"})
-      .then(res => console.log(res.data))
-      .catch(error => {
-        console.log(error)
-      });
+    axios.post('http://localhost:5000/favorites/add', {restaurant_id: 1, user_id: 2, status: "went"});
+    setCheckHeart(!checkHeart);
+    console.log("check heart is", checkHeart);
+  };
 
-    setCheckHeart(!checkHeart)
-    console.log("check heart is", checkHeart)
-    
+  const fetchFave = async () => {
+        const response = await axios.get(
+          'http://localhost:5000/favorites/'
+        );
+        console.log("response...", response.data)
+        setFavorites(response.data);
   }
+
+  useEffect(() => {
+    fetchFave();
+  },[favorites]);
+
 
   return (
     <div>
-      <input 
-        type="checkbox" 
-        name="heart" 
-        defaultChecked={setCheckHeart} 
-        onClick={e => toggleHeart()} 
+      <input
+        type="checkbox"
+        name="heart"
+        defaultChecked={setCheckHeart}
+        onClick={e => toggleHeart()}
       />Heart
+      <div>
+      test...
+      </div>
     </div>
   );
 };
