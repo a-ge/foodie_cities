f4a4ac2ac83f42d1a7bbb3592a19cf9d87674a1d
Ashley <amgesmundo@gmail.com>
2019-11-08 17:14:01 -0800

    Corrected back to objects for each city.

--- seed.js
+++ seed.js
@@ -1,31 +1,19 @@
 'use strict';
 
-require('dotenv').config();
 const fs = require('fs');
 
-const API_KEY = process.env.YELP_API_KEY;
 require('dotenv').config();
 const yelp = require('yelp-fusion');
 const API_KEY = process.env.YELP_API_KEY;
 const client = yelp.client(API_KEY);
 
-const locations = ['san francisco, ca', 'oakland, ca', 'berkeley, ca']
+const locations = ['San Francisco, CA', 'New York, NY', 'Atlanta, GA']
 
-for (let i=0; i < locations.length; i++){
-  client.search({
-    term: 'restaurants',
-    location: locations[i],
-    limit: 3,
-    sort_by: 'review_count'
-  }).then(response => {
-    console.log(response.jsonBody.businesses);
-  }).catch(e => {
-    console.log(e);
 function callYelp(i) {
     const response = client.search({
         term: 'restaurants',
         location: locations[i],
-        limit: 1,
+        limit: 10,
         sort_by: 'review_count'
    }).catch(e => {
      console.log(e);
@@ -37,18 +25,18 @@ async function createJSONFile() {
   const restObjArray = []
 
   for (let i=0; i < locations.length; i++) {
+    const dict = {}
     const resp = await callYelp(i)
-    restObjArray.push.apply(restObjArray, resp.jsonBody.businesses)
+    dict[locations[i]] = resp.jsonBody.businesses
+    restObjArray.push(dict)
   }
-  console.log(restObjArray);
 
   let data = JSON.stringify(restObjArray);
 
-  fs.writeFile('restaurants.json', data, (err) => {
+  fs.writeFile('./client/src/restaurants_data.json', data, (err) => {
       if (err) throw err;
       console.log('Data written to file');
   });
 };
-}
 
 createJSONFile()
