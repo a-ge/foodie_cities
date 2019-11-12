9686f06b442eca9fa44ab2c359cf1bc174043418
Ashley <amgesmundo@gmail.com>
2019-11-12 13:06:37 -0800

    Updated restaurants route and uncommented bookmarks route.

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,22 +2,26 @@ const router = require('express').Router();
 let Restaurant = require('../models/Restaurant');
 
 router.route('/').get((req, res) => {
-  foodiecities.find()
+  Restaurant.find()
   .then(restaurants => res.json(restaurants))
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
     const name = req.body.name;
+    const yelp_id = req.body.yelp_id;
     const image_url = req.body.image_url;
     const yelp_url = req.body.yelp_url;
-  
+    const isTop = true;
+
     const newRestaurant = new Restaurant({
       name,
+      yelp_id,
       image_url,
-      yelp_url
+      yelp_url,
+      isTop
     });
-  
+
     newRestaurant.save()
     .then(() => res.json('Restaurant added!'))
     .catch(err => res.status(400).json('Error: ' + err));
