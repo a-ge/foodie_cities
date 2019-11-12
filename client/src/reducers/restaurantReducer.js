197ef618027dacae7f4040f4d5618afbd7b82833
Ashley <amgesmundo@gmail.com>
2019-11-11 17:12:43 -0800

    Fixed zero in initial state objects.

--- client/src/reducers/restaurantReducer.js
+++ client/src/reducers/restaurantReducer.js
@@ -1,15 +1,15 @@
-import { 
-  GET_DB_RESTAURANTS, 
-  SET_LOADING, 
+import {
+  GET_DB_RESTAURANTS,
+  SET_LOADING,
   DB_RESTAURANTS_ERROR} from '../actions/types';
 
 const initialState = {
-  db_restaurants_city1:[{01: {yelp_id: "lJAGnYzku5zSaLnQ_T6_GQ", name: "Tartine", img: "tart_img", yelp: "tart_yelp", isTop: true}},
-                        {02: {yelp_id: "lJAGnYzku5zSaLrm_T6_GQ", name: "Lala", img: "lala_img", yelp: "lala_yelp", isTop: true}}],              
-  db_restaurants_city2:[{03: {yelp_id: "lJAGnYzku5zSaLwe_T6_GQ", name: "Donuts", img: "donuts_img", yelp: "donuts_yelp", isTop: true}},
-                        {04: {yelp_id: "lJAGnYzku5zSaLff_T6_GQ", name: "Chicken", img: "chicken_img", yelp: "chicken_yelp", isTop: true}}],
-  db_restaurants_city3:[{05: {yelp_id: "lJAGnYzku5zSaLid_T6_GQ", name: "Pizza", img: "pizza_img", yelp: "pizza_yelp", isTop: true}},
-                        {06: {yelp_id: "lJAGnYzku5zSaLsx_T6_GQ", name: "Burgers", img: "burgers_img", yelp: "burgers_yelp", isTop: true}}],
+  db_restaurants_city1:[{1: {yelp_id: "lJAGnYzku5zSaLnQ_T6_GQ", name: "Tartine", img: "tart_img", yelp: "tart_yelp", isTop: true}},
+                        {2: {yelp_id: "lJAGnYzku5zSaLrm_T6_GQ", name: "Lala", img: "lala_img", yelp: "lala_yelp", isTop: true}}],
+  db_restaurants_city2:[{3: {yelp_id: "lJAGnYzku5zSaLwe_T6_GQ", name: "Donuts", img: "donuts_img", yelp: "donuts_yelp", isTop: true}},
+                        {4: {yelp_id: "lJAGnYzku5zSaLff_T6_GQ", name: "Chicken", img: "chicken_img", yelp: "chicken_yelp", isTop: true}}],
+  db_restaurants_city3:[{5: {yelp_id: "lJAGnYzku5zSaLid_T6_GQ", name: "Pizza", img: "pizza_img", yelp: "pizza_yelp", isTop: true}},
+                        {6: {yelp_id: "lJAGnYzku5zSaLsx_T6_GQ", name: "Burgers", img: "burgers_img", yelp: "burgers_yelp", isTop: true}}],
   loading: false,
   error: null
 }
