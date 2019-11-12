a444bfff5f8e02bb0326b59a2ec99d1f9eeccd08
Ashley <amgesmundo@gmail.com>
2019-11-11 16:47:31 -0800

    Revert "Removed restaurant action reducer for now."
    This reverts commit f2d9078a7fa962c53f5eb9f50559501759a388f6.

---
+++ client/src/actions/restaurantActions.js
@@ -0,0 +1,31 @@
+// import { 
+//   GET_DB_RESTAURANTS, 
+//   SET_LOADING, 
+//   DB_RESTAURANTS_ERROR } from './types';
+
+// export const getDbRestaurants = () => async dispatch => {
+//   try {
+//     setLoading();
+
+//     const res = await fetch('http://localhost:5000/restaurants/');
+//     const data = await res.json();
+
+//     dispatch({
+//       type: GET_DB_RESTAURANTS,
+//       payload: data
+//     });
+
+//   } catch (err) {
+//     dispatch({
+//       type: DB_RESTAURANTS_ERROR,
+//       payload: err.response.data
+//     });
+//   }
+// };
+
+// // Set loading to true
+// export const setLoading = () => {
+//   return {
+//     type: SET_LOADING
+//   }
+// }
