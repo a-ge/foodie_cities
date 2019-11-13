8a1891637eab0bcd5cbd16e0746662a035dfa03b
Ashley <amgesmundo@gmail.com>
2019-11-12 16:47:05 -0800

    Removed restaurants route and model.

--- server.js
+++ server.js
@@ -19,11 +19,9 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-const restaurantsRouter = require('./routes/restaurants');
 const usersRouter = require('./routes/users');
 const bookmarksRouter = require('./routes/bookmarks');
 
-app.use('/restaurants', restaurantsRouter);
 app.use('/users', usersRouter);
 app.use('/bookmarks', bookmarksRouter);
 
