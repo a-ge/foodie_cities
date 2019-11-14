c807dab581c891e50d536a9d3a2d14293d357b30
Jessica <jessica.ho.jh@gmail.com>
2019-11-14 14:36:13 -0800

    made small changes

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    user_name: {type: String, required: true},
+    username: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true}
 });
