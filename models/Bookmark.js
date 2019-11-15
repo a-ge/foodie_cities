a7b24d017d4b4ff812c34d6d826317203f09f460
Ashley <amgesmundo@gmail.com>
2019-11-15 14:17:49 -0800

    Created route to add to user's bookmark array and add to bookmarks at the same time.

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema
 
-const bookmarksSchema = new Schema({
+const bookmarkSchema = new Schema({
     yelpId: {type: String, required: true},
     yelpUrl: {type: String, required: true},
     restaurantName: {type: String, required: true},
     imageUrl: {type: String, required: true}
 });
 
-const Bookmarks = mongoose.model('Bookmarks', bookmarksSchema);
+const Bookmark = mongoose.model('Bookmark', bookmarkSchema);
 
-module.exports = Bookmarks;
+module.exports = Bookmark;
--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -1,28 +1,45 @@
 const router = require('express').Router();
-let Bookmarks = require('../models/Bookmarks');
+let User = require('../models/User');
+let Bookmark = require('../models/Bookmark');
 
 router.route('/').get((req, res) => {
-    Bookmarks.find()
-    .then(bookmarks => res.json(bookmarks))
+    Bookmark.find()
+    .then(bookmark => res.json(bookmark))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
+    const user =  req.body.username;
     const yelpId = req.body.yelpId;
     const yelpUrl = req.body.yelpUrl;
     const restaurantName = req.body.restaurantName;
     const imageUrl = req.body.imageUrl;
 
-    const newBookmarks = new Bookmarks({
+    const bookmark = {
         yelpId,
         yelpUrl,
         restaurantName,
         imageUrl
-    });
+    }
 
-    newBookmarks.save()
-    .then(() => res.json('Bookmark added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    async function saving(bookmarkItem) {
+      let userBookmarkUpdate = await User.findOneAndUpdate(
+        {username: user},
+        {$push: {bookmarks: bookmarkItem}}
+      )
+
+      userBookmarkUpdate.save()
+        .then(() => res.json('User bookmarks updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+
+      const newBookmark = await new Bookmark(bookmarkItem);
+
+      newBookmark.save()
+        .then(() => res.json('Bookmark added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    }
+
+    saving(bookmark)
 });
 
 module.exports = router;
