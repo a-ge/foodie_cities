09bdf31da74a69068a3951fa2539c540ec406421
Ashley <amgesmundo@gmail.com>
2019-11-16 21:59:52 -0800

    Refactored db.json restaurants and CityButtons to fetch from component instead of redux.

--- client/src/components/restaurants/CityButtons.js
+++ client/src/components/restaurants/CityButtons.js
@@ -1,48 +1,51 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getDbRestaurants } from '../../actions/restaurantActions';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { getDbRestaurants } from '../../actions/restaurantActions';
 import Restaurants from './Restaurants';
 
 const CityButtons = () => {
 
   const [cityIndex, setCityIndex] = useState(0);
-
-  const citiesArray = useSelector((state) => state.restaurants.restaurants);
-
-  const dispatch = useDispatch();
-  const getRests = () => dispatch(getDbRestaurants());
+  const [cityRestaurants, setCityRestaurants] = useState([])
 
   useEffect(() => {
-    getRests();
-  }, [])
+    const fetchRestaurants = async () => {
+      const response = await fetch('http://localhost:4000/restaurants/');
+      const data = await response.json();
+            console.log("response", data);
+      setCityRestaurants(data);
+    }
+    fetchRestaurants()
+  }, []);
 
-  if (citiesArray === null) {
+  if (cityRestaurants === null) {
     return <div>No cities</div>
   }
   else {
-    const cityObject = citiesArray[cityIndex]
-
-    if (cityObject) {
-      const cityNameString = Object.keys(cityObject)[0]
-      const restaurants = cityObject[cityNameString]
-
+    // const cityObject = cityRestaurants[cityIndex]
+    // console.log("cityObject", cityObject)
+    // if (cityObject) {
+    //   const cityNameString = Object.keys(cityObject)[0]
+    //   console.log("cityNameString", cityNameString)
+    //   const restaurantsArray = cityObject[cityNameString]
+    //   console.log("restaurantsArray", restaurantsArray)
       return (
         <div>
           <ul>
             {
-              citiesArray.map((cityName, index) => {
-                    return <button key={index} onClick={() => setCityIndex(index)}> {Object.keys(cityName)[0]} </button>
+              cityRestaurants.map((cityData, index) => {
+                    return <button key={index} onClick={() => setCityIndex(index)}> {Object.keys(cityData)[0]} </button>
               })
             }
           </ul>
-          <Restaurants restaurants={restaurants} />
+          <Restaurants restaurants={cityRestaurants} />
         </div>
       )
     }
-    else {
-      return <div>No restaurants</div>
-    }
-  }
+    // else {
+    //   return <div>No restaurants</div>
+    // }
+  // }
 }
 
 export default CityButtons
