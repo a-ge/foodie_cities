7c65af552c8e5206390af4ef2af4d26ae2b50462
Ashley <amgesmundo@gmail.com>
2019-11-16 23:13:11 -0800

    Cleaned up Marker component console.logs and spacing.

--- client/src/components/Marker.js
+++ client/src/components/Marker.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 
-const Marker = ({mark}) => {
-  console.log("mark", mark)
+const Marker = ({ mark }) => {
   const [isClicked, setIsClicked] = useState(mark)
-
   console.log("checkbox", isClicked)
 
     return (
         <div className='marker'>
-          <input className='marker-button' type='checkbox' onClick={() => isClicked ? setIsClicked(false) : setIsClicked(true)}checked={isClicked}>
+          <input className='marker-button' type='checkbox' checked={isClicked}
+            onChange={() => isClicked ? setIsClicked(false) : setIsClicked(true)}>
           </input>
         </div>
     )
