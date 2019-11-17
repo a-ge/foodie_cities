91ad4ee7b52eda6f239b404ae5f7f563b97eb14c
Ashley <amgesmundo@gmail.com>
2019-11-17 00:12:56 -0800

    Added marker to restaurantItem.

--- client/src/components/restaurants/RestaurantItem.js
+++ client/src/components/restaurants/RestaurantItem.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import Marker from '../Marker';
 
 const RestaurantItem = ({ restaurant }) => {
+  const bookmarksSelector = useSelector((state) => state.bookmarks);
+  const bookmarksArray = bookmarksSelector.bookmarks
 
-  return (
-    <div>
+  if (!bookmarksArray) {
+    return <div>No bookmarks</div>
+  } else {
+    const idsArray = bookmarksArray.map(item => item.id)
+    console.log("idsArray", idsArray)
+    const mark = idsArray.includes(restaurant.id)
+    console.log("id", restaurant.id)
+
+    return (
       <div>
-        {restaurant.name}
+        <div>
+          {restaurant.name}
+        </div>
+        <div>
+          <img src={restaurant.image_url} alt="Not available" height="42" width="42" />
+        </div>
+        <div>
+          Rating: {restaurant.rating}
+        </div>
+        <div>
+          Review Count: {restaurant.review_count}
+        </div>
+        <Marker mark={mark}/>
+        <br/>
       </div>
-      <div>
-        <img src={restaurant.image_url} alt="Not available" height="42" width="42" />
-      </div>
-      <div>
-        Rating: {restaurant.rating}
-      </div>
-      <div>
-        Review Count: {restaurant.review_count}
-      </div>
-      <br/>
-    </div>
-  );
+    )
+  }
 };
 
 export default RestaurantItem
