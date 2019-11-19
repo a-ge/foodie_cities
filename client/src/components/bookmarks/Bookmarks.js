84218f1577e1b79162db5ad8572f47d6337b152c
Ashley <amgesmundo@gmail.com>
2019-11-18 23:10:58 -0800

    Cleaned up unused items.

--- client/src/components/bookmarks/Bookmarks.js
+++ client/src/components/bookmarks/Bookmarks.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getDbBookmarks } from '../../actions/bookmarkActions';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import BookmarkItem from './BookmarkItem';
 
 const Bookmarks = () => {
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getDbBookmarks } from '../../actions/bookmarkActions';
 import CityButtons from '../restaurants/CityButtons';
 import Bookmarks from '../bookmarks/Bookmarks';
