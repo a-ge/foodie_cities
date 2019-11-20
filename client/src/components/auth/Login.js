9fd4e124ded18ea7aa366056208a7afaa5ea0709
Jessica <jessica.ho.jh@gmail.com>
2019-11-20 13:08:47 -0800

    working on login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,29 +2,76 @@ import React from 'react';
 import { useSelector, connect } from 'react-redux';
 import { loginUser } from '../../actions/userActions';
 
+import axios from 'axios';
+// const fetch = require("node-fetch")
+
+
+// class EasyHTTP {
+//   async post(url, data) {
+//     const response = await fetch(url, {
+//       method: 'POST',
+//       headers: {'Content-type': 'application/json'},
+//       body: JSON.stringify(data)
+//     });
+
+//     const resData = await response.json();
+//     return resData;
+//   }
+// }
+
+// const http = new EasyHTTP;
+
 const Login = ({ loginUser }) => {
 
     const isLogged = useSelector((state) => state.users.isLogged);
     console.log("login isLogged", isLogged)
 
-    const onSubmit = async (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
 
         const formUsername = document.getElementById('username').value;
         const formPassword = document.getElementById('password').value;
 
-        console.log('grabbed', formUsername, formPassword)
+        const formData = {"username": formUsername, "password": formPassword}
 
-        const res = await fetch(`http://localhost:5000/users/login`)
-        // .then(loginUser())
-        // .catch(err => console.log(err))
 
-        if (res.status === 400) {
-          console.log('wrong username or password!')
-        } else {
-          loginUser()
-        }
+        function postUserFormData() {
+
+
+
+          axios({
+            method: 'post',
+            url: '/users/login',
+            data: {username: formUsername, password: formPassword}
+          });
+
+
+          // const config = {
+          //   headers: {
+          //     'Content-Type': 'application/json'
+          //   }
+          // };
+
+          // axios.post('http://localhost:5000/users/login', formData, config)
+          // .then((res) =>  console.log("data is", res.data))
+          // .catch((err) => console.log(err))
+
+
+
+
+      
+          //   http.post('http://localhost:5000/users/login', formData)
+          //   .then(data =>  console.log("data is", data))
+          //   // .then(loginUser())
+          //   .catch(err => console.log(err))
+          // }
+
+            
       };
+      postUserFormData()
+    }
+
+      
     
   return (
     <div>
