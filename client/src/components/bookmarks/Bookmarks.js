3df3cbf9b3e7175be137f157dd788b8c4e3f99ba
Ashley <amgesmundo@gmail.com>
2019-11-21 13:52:59 -0800

    Displaying bookmarks correctly.

--- client/src/components/bookmarks/Bookmarks.js
+++ client/src/components/bookmarks/Bookmarks.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import BookmarkItem from './BookmarkItem';
 
 const Bookmarks = () => {
   const bookmarksArray = useSelector((state) => state.bookmarks.bookmarks);
+  const [ bookmarks, setBookmarks] = useState([]);
+
+  const getBookmarks = () => {
+    let table = [];
+
+    for (let i=0; i < bookmarksArray.length; i++){
+      if (bookmarksArray[i]) {
+        table.push(bookmarksArray[i].restaurants)
+      }
+    }
+
+    setBookmarks(table.flat())
+  }
+
+  useEffect(() => {
+    getBookmarks();
+  }, [bookmarksArray])
 
   return (
     <div>
       BOOKMARKS
       <ul>
         {
-          bookmarksArray.map((bookmark, index) =>
+          bookmarks.map((bookmark, index) =>
             <BookmarkItem bookmark={bookmark} key={index} />)
         }
       </ul>
