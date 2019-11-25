c8726935d2f84a82bb0377f65da8213670311479
Ashley <amgesmundo@gmail.com>
2019-11-25 15:09:30 -0800

    Removed semicolon.

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 let Restaurant = require('../models/Restaurant');
 
 router.route('/').get((req, res) => {
-  Restaurant.find({});
+  Restaurant.find({})
   .then(restaurants => res.json(restaurants))
   .catch(err => res.status(400).json('Error: ' + err));
 });
