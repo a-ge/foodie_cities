6eb5a61dc447540bcfd9efeea5c1ed6baaa77fe6
Ashley <amgesmundo@gmail.com>
2019-11-26 13:18:33 -0800

    Spacing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Login from './components/auth/Login';
 import Logout from './components/auth/Logout';
 import Register from './components/auth/Register';
-
-import { Provider } from 'react-redux';
-import store from './store';
 
 import Container from 'react-bootstrap/Container';
 
@@ -17,7 +17,7 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Fragment>
-        <Navbar/>
+          <Navbar/>
           <Container>
             <Switch>
               <Route exact path='/' component={Home}/>
