88d93c9692c8c940bd301630dfe6aadd25fbd673
Ashley <amgesmundo@gmail.com>
2019-12-03 14:44:16 -0800

    Removed loading and error action/reducer.

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,18 +1,11 @@
 import {
   REGISTER_USER,
-  REGISTER_ERROR,
   LOGIN_USER,
-  LOGIN_ERROR,
-  LOGOUT_USER,
-  LOGOUT_ERROR,
-  SET_LOADING
+  LOGOUT_USER
 } from './types';
 
 // Register User to server
 export const addUser = user => async dispatch => {
-  try {
-    setLoading();
-
     await fetch('http://localhost:3000/users/add', {
       method: 'POST',
       body: JSON.stringify(user),
@@ -25,53 +18,19 @@ export const addUser = user => async dispatch => {
       type: REGISTER_USER,
       payload: user.username
     });
-
-  } catch (err) {
-    dispatch({
-      type: REGISTER_ERROR,
-      payload: err.response
-    });
-  }
 };
 
 // Login the user if passed check
 export const loginUser = (username) => dispatch => {
-  try {
-    setLoading();
-
-    dispatch({
-      type: LOGIN_USER,
-      payload: username
-    });
-
-  } catch (err) {
-    dispatch({
-      type: LOGIN_ERROR,
-      payload: err.response.statusText
-    });
-  }
+  dispatch({
+    type: LOGIN_USER,
+    payload: username
+  });
 };
 
 // Logout the user
 export const logoutUser = () => dispatch => {
-  try {
-    setLoading();
-
-    dispatch({
-      type: LOGOUT_USER
-    });
-
-  } catch (err) {
-    dispatch({
-      type: LOGOUT_ERROR,
-      payload: err.response.statusText
-    });
-  }
-};
-
-  // Set loading to true
-export const setLoading = () => {
-  return {
-    type: SET_LOADING
-  };
+  dispatch({
+    type: LOGOUT_USER
+  });
 };
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,17 +1,12 @@
-import { REGISTER_USER,
-         REGISTER_ERROR,
-         LOGIN_USER,
-         LOGIN_ERROR,
-         LOGOUT_USER,
-         LOGOUT_ERROR,
-         SET_LOADING
+import {
+  REGISTER_USER,
+  LOGIN_USER,
+  LOGOUT_USER
 } from '../actions/types';
 
 const initialState = {
-user: null,
-loading: false,
-error: null,
-isLogged: false
+  user: null,
+  isLogged: false
 };
 
 export default (state = initialState, action) => {
@@ -19,56 +14,20 @@ export default (state = initialState, action) => {
 
     case REGISTER_USER:
       return {
-          user: action.payload,
-          loading: false,
-          error: null,
-          isLogged: true
-      };
-
-    case REGISTER_ERROR:
-      console.error(action.payload);
-      return {
-          ...state,
-          error: action.payload,
-          loading: false
+        user: action.payload,
+        isLogged: true
       };
 
     case LOGIN_USER:
       return {
         user: action.payload,
-        loading: false,
-        error: null,
         isLogged: true
-      };
-
-    case LOGIN_ERROR:
-      console.error(action.payload);
-      return {
-          ...state,
-          error: action.payload,
-          loading: false
       };
 
     case LOGOUT_USER:
       return {
         user: null,
-        loading: false,
-        error: null,
         isLogged: false
-      };
-
-    case LOGOUT_ERROR:
-      console.error(action.payload);
-      return {
-          ...state,
-          error: action.payload,
-          loading: false
-      };
-
-    case SET_LOADING:
-      return {
-          ...state,
-          loading: true
       };
 
     default:
