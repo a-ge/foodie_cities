5e4b5b8e78a723f930838422e70a9128f1ff9bbb
Ashley <amgesmundo@gmail.com>
2019-12-03 14:25:53 -0800

    Removed loading.

--- client/src/components/restaurants/Restaurants.js
+++ client/src/components/restaurants/Restaurants.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import RestaurantItem from './RestaurantItem';
 
 import Container from 'react-bootstrap/Container';
@@ -7,13 +6,10 @@ import CardDeck from 'react-bootstrap/CardDeck';
 import Modal from 'react-bootstrap/Modal'
 
 const Restaurants = ({ cityRestaurants, cityName }) => {
-  const loading = useSelector((state) => state.users.loading);
 
   return (
     <Modal.Body style={{maxheight: 'calc(100vh - 210px)', overflowy: 'auto'}}>
       <Container>
-        {
-          loading ? <div>loading...</div> :
             <CardDeck>
               {
                 cityRestaurants.map((restaurant, index) =>
@@ -21,7 +17,6 @@ const Restaurants = ({ cityRestaurants, cityName }) => {
                 )
               }
             </CardDeck>
-        }
       </Container>
     </Modal.Body>
   );
