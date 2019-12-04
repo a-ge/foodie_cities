ccbe169ede253646132e9be2dfc5d923bc74763a
Jessica <jessica.ho.jh@gmail.com>
2019-12-03 16:32:05 -0800

    moved port after production line

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -35,6 +35,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
