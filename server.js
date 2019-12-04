57ee81e9d2355a4c4e298f2ecd92e7d967604633
Jessica <jessica.ho.jh@gmail.com>
2019-12-03 16:16:02 -0800

    preparing for herkoku deployment

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+
 var cors = require('cors');
 
 require('dotenv').config();
@@ -26,6 +28,13 @@ const restaurantsRouter = require('./routes/restaurants');
 app.use('/users', usersRouter);
 app.use('/bookmarks', bookmarksRouter);
 app.use('/restaurants', restaurantsRouter);
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
